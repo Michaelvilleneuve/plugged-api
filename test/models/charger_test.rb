9afdd7ab2e214f7cea8acf95582aa1cd7befd6dc
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-05-27 11:15:47 +0200

    Annotations

--- test/models/charger_test.rb
+++ test/models/charger_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: chargers
+#
+#  id         :integer          not null, primary key
+#  lat        :float
+#  lng        :float
+#  user_id    :integer
+#  plug_type  :integer
+#  hour_price :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ChargerTest < ActiveSupport::TestCase
